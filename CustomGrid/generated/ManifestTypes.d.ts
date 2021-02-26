441e6bc78c90ecf408ab7d6d0d4f4b2786168e8b
Fernando Bo Gimenez <fernando.bo21@gmail.com>
2021-02-26 15:23:14 +0100

    Changed automatically the manifest types

--- CustomGrid/generated/ManifestTypes.d.ts
+++ CustomGrid/generated/ManifestTypes.d.ts
@@ -1,11 +1,12 @@
 /*
- * This is auto generated from the ControlManifest.Input.xml file
- */
+*This is auto generated from the ControlManifest.Input.xml file
+*/
 
 // Define IInputs and IOutputs Type. They should match with ControlManifest.
 export interface IInputs {
+    gridSelect: ComponentFramework.PropertyTypes.EnumProperty<"FluentUI" | "MaterialUI">;
     sampleDataSet: ComponentFramework.PropertyTypes.DataSet;
 }
-
 export interface IOutputs {
+    gridSelect?: string;
 }
