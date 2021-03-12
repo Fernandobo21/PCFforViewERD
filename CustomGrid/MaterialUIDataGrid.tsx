d1afc19e493b6c820527f81871ff670eb7be6613
Fernando Bo Gimenez <fernando.bo21@gmail.com>
2021-03-12 15:54:48 +0100

    Generating rows dynamically

--- CustomGrid/MaterialUIDataGrid.tsx
+++ CustomGrid/MaterialUIDataGrid.tsx
@@ -7,13 +7,12 @@ export class MaterialUIDataGrid extends React.Component<{}> {
   constructor(props:any) {
     super(props);
     this._generateColumns(props.keyCRM, props.columnsCRM);
-  }
+    this._generateRows(props.keyCRM, props.valueCRM);
+  } 
   public render() {
-    this._generateRows();
-    
     return (<div style={{width: '1500px', height: '400px'}}><DataGrid rows={this._rows} columns={this._columns} pageSize={5} checkboxSelection /></div>);
   } 
-  public _generateColumns(keyCRM:[], columnsCRM:[])
+  public _generateColumns(keyCRM:any, columnsCRM:any)
   {
     // keyCRM.map((value: string, index: number) => {
     //   this._columns.push(
@@ -45,19 +44,19 @@ export class MaterialUIDataGrid extends React.Component<{}> {
       },
     ];
   }
-  public _generateRows()
+  public _generateRows(keyCRM:any, valueCRM:any)
   {
-    this._rows = [
-      { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-      { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-      { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-      { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-      { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-      { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-      { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-      { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-      { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
+    this._rows = [];
+    keyCRM.map((value: string, index: number) => {
+       this._rows.push(
+         {
+           id: value,
+           lastName: "Bo " + index,
+           firstName: "Fernando " + index,
+           age: index
+         }
+       );
+     });
   }
 };
 export default MaterialUIDataGrid;
