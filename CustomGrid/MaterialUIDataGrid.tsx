25795bd84df2900272860492c5657006c681fdc3
Fernando Bo Gimenez <fernando.bo21@gmail.com>
2021-03-16 15:58:25 +0100

    Dynamically changes for the values

--- CustomGrid/MaterialUIDataGrid.tsx
+++ CustomGrid/MaterialUIDataGrid.tsx
@@ -6,54 +6,61 @@ export class MaterialUIDataGrid extends React.Component<{}> {
   private _columns:ColDef[];
   constructor(props:any) {
     super(props);
-    this._generateColumns(props.keyCRM, props.columnsCRM);
-    this._generateRows(props.keyCRM, props.valueCRM);
+    this._generateColumns(props.columnsCRM, props.typeCRM);
+    this._generateRows(props.valueCRM);
   } 
   public render() {
     return (<div style={{width: '1500px', height: '400px'}}><DataGrid rows={this._rows} columns={this._columns} pageSize={5} checkboxSelection /></div>);
   } 
-  public _generateColumns(keyCRM:any, columnsCRM:any)
+  public _generateColumns(columnsCRM:any, typeCRM:any)
   {
-    // keyCRM.map((value: string, index: number) => {
-    //   this._columns.push(
-    //     {
-    //       field: value,
-    //       headerName: columnsCRM[index],
-    //       width: 70
-    //     }
-    //   );
-    // });
-    this._columns = [
-      { field: 'id', headerName: 'ID', width: 70 },
-      { field: 'firstName', headerName: 'First name', width: 130 },
-      { field: 'lastName', headerName: 'Last name', width: 130 },
-      {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-      },
-      {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params: ValueGetterParams) =>
-          `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-      },
-    ];
+    this._columns = [];
+    debugger;
+    columnsCRM.map((value: string, index: number) => {
+      this._columns.push(
+        {
+          field: value,
+          headerName: this.capitalizeFirstLetter(value),
+          width: 130,
+          type: typeCRM[index]
+        }
+      );
+    });
+    // this._columns = [
+    //   { field: 'id', headerName: 'ID', width: 70 },
+    //   { field: 'firstName', headerName: 'First name', width: 130 },
+    //   { field: 'lastName', headerName: 'Last name', width: 130 },
+    //   {
+    //     field: 'age',
+    //     headerName: 'Age',
+    //     type: 'number',
+    //     width: 90,
+    //   },
+    //   {
+    //     field: 'fullName',
+    //     headerName: 'Full name',
+    //     description: 'This column has a value getter and is not sortable.',
+    //     sortable: false,
+    //     width: 160,
+    //     valueGetter: (params: ValueGetterParams) =>
+    //       `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+    //   },
+    // ];
   }
-  public _generateRows(keyCRM:any, valueCRM:any)
+  public capitalizeFirstLetter(value:string) {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
+  public _generateRows(valueCRM:any)
   {
     this._rows = [];
-    keyCRM.map((value: string, index: number) => {
+    valueCRM.map((value:any, index: number) => {
+      debugger;
        this._rows.push(
          {
-           id: value,
-           lastName: "Bo " + index,
-           firstName: "Fernando " + index,
-           age: index
+           id: value.id,
+           lastName: value.lastName,
+           firstName: value.firstName,
+           age: value.age
          }
        );
      });
